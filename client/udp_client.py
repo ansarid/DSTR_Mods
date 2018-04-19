b0e5ce1e99f39b6850c7b005a9662e8e6f67388e
Daniyal Ansari <daniyal.s.ansari+github@gmail.com>
2018-04-18 21:38:52 -0500

    Update udp_client.py

--- client/udp_client.py
+++ client/udp_client.py
@@ -1,18 +1,24 @@
 import socket
 import serial
-from time import sleep
 
-IP = "192.168.1.1"
-PORT = 3553
+IP = "192.168.1.1"	# Beagle Bone AP Default Gateway IP
+PORT = 3553		# Beagle Bone DSTR Server Port
 
-ser = serial.Serial('/dev/ttyUSB0', 38400, timeout=1)
-print(ser.name)
+ser = serial.Serial('/dev/ttyUSB0', 38400, timeout=1)	# (port, baud, timeout)
 
-while True:
+print(ser.name)		# /dev/ttyUSB0
 
+while True: # Run Forever
+
+	# Number of Byes to Read
+	
 	data = ser.read(12)
 
+	# Display Data
+	
 	print("Data: ", data ,"\t",len(data), "Bytes |")
 
+	# Send Data
+	
 	clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	clientSock.sendto(data, (IP, PORT))
